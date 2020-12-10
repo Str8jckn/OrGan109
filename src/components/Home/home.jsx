afb947a0153089d6fe5f760455a047c2d6f7bccf
Jerome Brown <jaebrownjr@gmail.com>
2020-12-09 19:00:22 -0700

    change

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Home extends Component {
   state = {};
   render() {
-    return <h1>Welcome</h1>;
+    return <h1>Welcome To My Little Party in your Honor, LEROY!</h1>;
   }
 }
 
