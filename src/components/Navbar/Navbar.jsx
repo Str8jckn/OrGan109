42490eca09794353ba7adec00990403047e352ee
Jerome Brown <jaebrownjr@gmail.com>
2020-12-20 20:12:53 -0700

    109again

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav
+        className="navbar navbar-default navbar-fixed-top"
         className="navbar navbar-expand-lg navbar-dark"
         style={{ backgroundColor: "#015d87" }}
       >
